b5ca4fc17588124c2adab59d3378bbf476dc8e94
Mathieu <m.bidoyen@gmail.com>
2024-03-11 14:58:59 +0100

    Initial Commit

---
+++ src/test/java/fr/mathieu/SpringBootAuthentication/SpringBootAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mathieu.SpringBootAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
